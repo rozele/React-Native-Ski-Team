01c2e3c3904162118f28e225d4cfc1f8730a4be1
Dave Voyles <dnvoyles@gmail.com>
2017-11-15 12:08:59 -0700

    added bib color modal

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ export default class PizzaTranslator extends Component {
     this.aGenderOptions = ['Male', 'Female'];
     this.aLaneOptions   = [1,2,3,4,5,6     ];
     this.aHeatOptions   = [1,2,3,4         ];
+    this.aBibOptions    = ['Red', 'Blue', 'Green', 'Purple, Orange', 'Yellow', 'Black' ];
 
     this.nHeight     = 80;
     this.nPadding    = 40;
@@ -27,6 +28,8 @@ export default class PizzaTranslator extends Component {
     this.borderColor = 'black';
   }
   
+  /** Event handlers for button and modals
+   * ------------------------------------ */
   _handlePBtnPress() {
     // TODO: POST content from input fields to DB
     console.log('Pressed!');
@@ -47,19 +50,29 @@ export default class PizzaTranslator extends Component {
     console.log('Lane selected' + this.state.heatNum);
   }
 
+  _handleBibSelect(idx, value){
+    this.state.bibCol = value;
+    console.log('Lane selected' + this.state.bibCol);
+  }
+
   render() {
     return (
       // need at least 40 to prevent text from hidding top of phone in simulator
       <View style={{padding: this.nPadding}}>
-        <TextInput
-        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
-        placeholder="Bib Color"
-        onChangeText={(bibCol) => this.setState({bibCol})}
-        />
+              
         <TextInput 
         style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Bib #"
         onChangeText={(bibNum) => this.setState({bibNum})}
+        />
+
+        <ModalDropdown
+        defaultValue="Bib Color"
+        onSelect={(idx, value) => this._handleBibSelect(idx, value)}
+        options= {this.aBibOptions}
+        textStyle={{fontSize: this.nFontSize}}
+        style={{margin: this.nMargin, borderWidth: this.borderWidth, borderColor: this.borderColor, borderWidth: this.borderWidth, padding:10, height:this.nHeight}}        
+        dropdownTextStyle= {{fontSize: 20, color: 'black'}} 
         />
   
         <ModalDropdown
@@ -98,10 +111,7 @@ export default class PizzaTranslator extends Component {
 
       </View>
     );
-
-
   }
 }
-
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
