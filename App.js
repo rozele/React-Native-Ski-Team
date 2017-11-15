2666853507564a812f00a3906b4b0e79c4e74c03
Dave Voyles <dnvoyles@gmail.com>
2017-11-15 10:13:48 -0700

    Added margins between input fields

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ export default class PizzaTranslator extends Component {
     this.nHeight     = 80;
     this.nPadding    = 40;
     this.nFontSize   = 50;
+    this.nMargin     = 15;
     this.borderWidth = 1;
     this.borderColor = 'black';
   }
@@ -26,45 +27,41 @@ export default class PizzaTranslator extends Component {
     console.log('Pressed!');
   }
 
-
   render() {
     return (
       // need at least 40 to prevent text from hidding top of phone in simulator
       <View style={{padding: this.nPadding}}>
         <TextInput
-        style={{height: this.nHeight, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
+        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Bib Color"
         onChangeText={(bibCol) => this.setState({bibCol})}
         />
         <TextInput 
-        style={{height: this.nHeight, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
+        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Bib #"
         onChangeText={(bibNum) => this.setState({bibNum})}
         />
         <TextInput 
-        style={{height: this.nHeight, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
+        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Heat #"
         onChangeText={(heatNum) => this.setState({heatNum})}
         />
         <TextInput 
-        style={{height: this.nHeight, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
+        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Lane #"
         onChangeText={(laneNum) => this.setState({laneNum})}
         />
         <TextInput 
-        style={{height: this.nHeight, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
+        style={{height: this.nHeight, margin: this.nMargin, fontSize: this.nFontSize, borderColor: this.borderColor, borderWidth: this.borderWidth}}
         placeholder="Gender"
         onChangeText={(gender) => this.setState({gender})}
         />
 
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.bibCol.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-
-        <Button
-          containerStyle={{padding:10, height:45, overflow:'hidden', borderRadius:6, backgroundColor: 'black'}}
-          style={{fontSize: 20, color: 'white'}}>
-          Submit
+        <Button          
+        containerStyle={{padding:10, height:45, margin: 80, overflow:'hidden', borderRadius:6, backgroundColor: 'black'}}
+        style={{fontSize: 24, color: 'white'}}
+        onPress={() => this._handlePress()} >
+        Submit
         </Button>
 
       </View>
