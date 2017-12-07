bc98f3b0b55b5099d32c6a11b4fd1d0e77fdcc67
Dave Voyles <dnvoyles@gmail.com>
2017-12-06 19:14:11 -0500

    Added App Center

---
+++ ios/Pods/Target Support Files/Pods-SkiApp-tvOSTests/Pods-SkiApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkiApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_SkiApp_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-SkiApp/Pods-SkiApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkiApp : NSObject
+@end
+@implementation PodsDummy_Pods_SkiApp
+@end
