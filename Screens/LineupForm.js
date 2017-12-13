eb2df6c771118c33854ea56bb8ab1b3d77c2f7c6
rdc64 <chrobles@microsoft.com>
2017-12-13 16:10:07 -0500

    refactor lineupform. TODO: display captured images. send data to storage.

--- Screens/LineupForm.js
+++ Screens/LineupForm.js
@@ -1,249 +1,146 @@
 import React, { Component } from 'react'
-import Button from 'react-native-button'
-import ModalDropdown from 'react-native-modal-dropdown'
-import { Col, Row, Grid } from 'react-native-easy-grid'
 import {
+  Button,
+  Picker,
   StyleSheet,
-  TouchableHighlight,
   Text,
   View,
   Image,
-  ScrollView
 } from 'react-native'
-import styles from '../Styles/LineupFormStyles'
 
 class LineupForm extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.title}`,
-  });
-  
+  })
+
   constructor(props) {
     super(props)
-    
+
+    propState = props.navigation.state.params.props
     this.state = {
-      nLane: [1, 2, 3, 4, 5, 6],
-      bibCol: ['Red', 'Green', 'Blue', 'White', 'Yellow', 'Black']
+      codex:         propState.codex,
+      gender:        propState.gender,
+      temperature:   propState.temperature,
+      precipitation: propState.precipitation,
+      course:        propState.course,
+      racers:        propState.racers,
+      position:      propState.position,
+      images: [],
+      lane1: '',
+      lane2: '',
+      lane3: '',
+      lane4: '',
+      lane5: '',
+      lane6: '',
     }
-    
-    // Modal options
-    this.bibOpt = ['Red', 'Green', 'Blue', 'White', 'Yellow', 'Black']
-    this.laneOpt = [1, 2, 3, 4, 5, 6]
-
-    // NOTE: fetch on iOS ONLY accepts https requests.
-    // See : https://stackoverflow.com/questions/38418998/react-native-fetch-network-request-failed
-    this.sAzureUrl = "https://sportstrackinglogger.azurewebsites.net/?"
-  }
-
-  /**
-   * Loop through each item in state and create a string formatted correctly for Azure Func 
-   */
-  createUrlToPost() {
-    var myString = ''
-
-    for (var key in this.state.nLane) {
-      // Get values in this.state object
-      if (this.state.nLane.hasOwnProperty(key)) {
-        var obj = this.state.bibCol[key]
-        myString += 'laneID' + key + '=' + obj + '&'
-      }
-    }
-
-    // Remove the final & from the end of the string
-    var pos = myString.lastIndexOf('&')
-    var newString = myString.substring(0, pos) + "" + myString.substring(pos + 1)
-    console.log(newString)
-
-    return newString
-  }
-
-  /** 
-   * Is actually a GET operation, as that is what Azure Functions accept to post data 
-   */
-  postDataToAzureFuncAsync() {
-    var _sAzureFuncParams = this.createUrlToPost()
-    var _sAzureUrl = this.sAzureUrl + _sAzureFuncParams
-
-    console.log('url:', _sAzureUrl)
-
-    return fetch(_sAzureUrl)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-        return responseJson
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
-
-  /**
-   *  NavBar at bottom of screen
-   */
-  // static navigationOptions = {
-  //   tabBarLabel: 'FORMVAR',
-  //   // tabBarIcon: ({ tintColor }) => (
-  //   //   <Image
-  //   //     source={require('../React-Native-Ski-Team/images/chat-icon.png')}
-  //   //     style={[styles.icon, { tintColor: tintColor }]}
-  //   //   />
-  //   // )
-  // }
-
-  // Event Handlers
-  // --------------------------------------------------
-  _handleBtnPress() {
-    //this.postDataToAzureFuncAsync()
-    this.postDataToAzureFuncAsync()
-    console.log(this.props)
-  }
-
-  /**
-   * Stores bib color to a lane.
-   * Removes current value from array, so that it does not appear when next button is selected.
-   * @param {number} idx   - Index of currently selected item in array
-   * @param {*}      value - Currently selected item in array
-   */
-  _handleBibSelect(idx, value, nLane) {
-    this.state.bibCol[nLane - 1] = value
-    this.state.nLane[nLane - 1] = nLane
-    this.bibOpt.splice(idx, 1)
   }
 
   render() {
     const { navigate } = this.props.navigation
     return (
-      <Grid>
-        {/* --------- Column------------- */}
-        <Col size={60} style={styles.columnLeft} >
-          <View style={styles.imageView}>
-            <Image source={require('../images/StartLineImg.jpg')}
-              style={styles.image}
-            />
-          </View>
-          <Row>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 1)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 2)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 3)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 4)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 5)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="LISTVAR"
-                onSelect={(idx, value) => this._handleBibSelect(idx, value, 6)}
-                options={this.bibOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.modalText}
-                style={styles.modalLeft}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-          </Row>
-        </Col>
-        {/* --------- Column------------- */}
-        <Col size={35}>
-          <Row style={styles.titleRow}>
-            <View>
-              <Text style={styles.title}>
-                {this.props.navigation.state.params.title}
-              </Text>
-            </View>
-          </Row>
-          <Row>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="Race: R9999"
-                onSelect={(idx, value) => this._handleLaneSelect(idx, value)}
-                options={this.laneOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.dropdownText}
-                style={styles.modalRight}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-            <Col style={styles.modalCol}>
-              <ModalDropdown
-                defaultValue="Phase: Heat1"
-                onSelect={(idx, value) => this._handleLaneSelect(idx, value)}
-                options={this.laneOpt}
-                dropdownStyle={styles.dropdown}
-                textStyle={styles.dropdownText}
-                style={styles.modalRight}
-                dropdownTextStyle={styles.dropdownText}
-              />
-            </Col>
-          </Row>
-          <Row style={styles.cameraRow}>
-            <View style={styles.cameraView}>
-              <Button onPress={() => navigate('ImageCapture')}>
-                <Image source={require('../images/CameraIcon.png')}
-                  style={styles.cameraImage} />
-              </Button>
-            </View>
-          </Row>
-          <Row style={styles.goRow}>
-            <View style={styles.goView}>
-              <Button
-                containerStyle={styles.goButton}
-                style={{ fontSize: 24, color: 'white' }}
-                onPress={() => this._handleBtnPress()} >
-                GO
-                </Button>
-            </View>
-          </Row>
-        </Col>
-      </Grid>
+      <View style={styles.container}>
+        <View style={styles.imageView}>
+          <Text style={styles.imageText}>
+            Use the camera to capture images
+          </Text>
+        </View>
+        <Button
+          onPress={() =>
+            navigate('ImageCapture')
+          }
+          style={styles.button}
+          title='Camera'
+        />
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 1:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 2:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 3:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 4:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 5:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Lane 6:
+          </Text>
+          <Picker style={styles.input}
+          />
+        </View>
+        <Button
+          onPress={() =>
+            console.log(this.state)
+          }
+          style={styles.button}
+          title='Submit'
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageView: {
+    flex: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageText: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  text: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'left',
+    padding: 30,
+  },
+  input: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+  },
+})
 
 export default LineupForm
--- Screens/NewRace.js
+++ Screens/NewRace.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {
+  Alert,
   Button,
-  Dimensions,
   Picker,
   StyleSheet,
   Text,
@@ -10,64 +10,130 @@ import {
 } from 'react-native'
 
 class NewRace extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      codex: '',
+      gender: 'Female',
+      temperature: '',
+      precipitation: 'None',
+      course: '',
+      racers: 6,
+      position: 'Start Gate',
+    }
+  }
+
   static navigationOptions = {
     title: 'New Race'
   }
 
-  onLayeout(e) {
-    const { width, height } = Dimensions.get('window')
-    console.log(width, height)
+  _handleBtnPress() {
+    const { navigate } = this.props.navigation
+    if (this.state.codex       === '' ||
+        this.state.temperature === '' ||
+        this.state.course      === ''
+       ) {
+      Alert.alert('Error: please enter a value for all text fields.')
+    }
+    else {
+      navigate('LineupForm', { title: this.state.position, props: this.state })
+    }
   }
 
   render() {
-    const { navigate } = this.props.navigation
     return (
-      <View
-        onLayout={this.onLayout}
-        style={styles.container}
-        >
+      <View style={styles.container}>
         <View style={styles.row}>
-          <Text style={styles.text}>Race Codex:</Text>
+          <Text style={styles.text}>
+            Race Codex:
+          </Text>
           <TextInput
             style={styles.input}
+            value={this.state.codex}
+            placeholder={'N/a'}
+            onChangeText={(text) => this.setState({codex: text})}
           />
         </View>
         <View style={styles.row}>
-          <Text style={styles.text}>Gender:</Text>
+          <Text style={styles.text}>
+            Gender:
+          </Text>
           <Picker
             style={styles.input}
-          />
+            selectedValue={this.state.gender}
+            onValueChange={(itemValue, itemIndex) => this.setState({gender: itemValue})}
+            >
+            <Picker.Item label='Female' value='female' />
+            <Picker.Item label='Male'   value='male'   />
+          </Picker>
         </View>
         <View style={styles.row}>
-          <Text style={styles.text}>Temp:</Text>
+          <Text style={styles.text}>
+            Temp:
+          </Text>
           <TextInput
             style={styles.input}
+            value={this.state.temperature}
+            placeholder={'N/a'}
+            onChangeText={(text) => this.setState({temperature: text})}
           />
         </View>
         <View style={styles.row}>
-          <Text style={styles.text}>Precip:</Text>
+          <Text style={styles.text}>
+            Precipitation:
+          </Text>
           <Picker
             style={styles.input}
-          />
+            selectedValue={this.state.precipitation}
+            onValueChange={(itemValue, itemIndex) => this.setState({precipitation: itemValue})}
+            >
+            <Picker.Item label='None' value='none' />
+            <Picker.Item label='Rain' value='rain' />
+            <Picker.Item label='Snow' value='snow' />
+          </Picker>
         </View>
         <View style={styles.row}>
-          <Text style={styles.text}>Course:</Text>
+          <Text style={styles.text}>
+            Course:
+          </Text>
           <TextInput
             style={styles.input}
+            value={this.state.course}
+            placeholder={'N/a'}
+            onChangeText={(text) => this.setState({course: text})}
           />
         </View>
-        <View style={styles.row}>
-          <Text style={styles.text}>Racers:</Text>
+        <View style={styles.row} enabled={false}>
+          <Text style={styles.text}>
+            Racers:
+          </Text>
           <Picker
             style={styles.input}
-          />
-      </View>
-      <Button
-        onPress={() =>
-          navigate('PositionSelector')
-        }
-        style={styles.button}
-        title='Position >'
+            selectedValue={this.state.racers}
+            onValueChange={(itemValue, itemIndex) => this.setState({racers: itemValue})}
+            >
+            <Picker.Item label='6' value='6' />
+          </Picker>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Your position:
+          </Text>
+          <Picker
+            style={styles.input}
+            selectedValue={this.state.position}
+            onValueChange={(itemValue, itemIndex) => this.setState({position: itemValue})}
+            >
+            <Picker.Item label='Start Gate' value='Start Gate' />
+            <Picker.Item label='Split'      value='Split'      />
+            <Picker.Item label='Hole'       value='Hole'       />
+            <Picker.Item label='Finish'     value='Finish'     />
+          </Picker>
+        </View>
+        <Button
+          onPress={() => this._handleBtnPress()}
+          style={styles.button}
+          title='Next >'
         />
       </View>
     )
@@ -89,10 +155,10 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 20,
     textAlign: 'left',
-    padding: 30
+    padding: 30,
   },
   input: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
