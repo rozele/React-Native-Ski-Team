51166ba9b7eee444e1c29370a6f0ce64b8299bef
Eric Rozell <erozell@outlook.com>
2017-12-14 19:57:37 -0500

    CR stuff

---
+++ Screens/ImageCapture.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import Camera from 'react-native-camera'
+
+class ImageCapture extends Component {
+  state = {
+    images: []
+  }
+
+  static navigationOptions = {
+    title: 'Camera'
+  }
+
+  takePicture() {
+    const options = {}
+    this.camera.capture({ metadata: options })
+      .then((data) => {
+        console.log('image captured')
+        console.log('data: ', data)
+        this.props.navigation.state.params.setImages(data)
+      })
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    const { goBack } = this.props.navigation
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={
+            this.takePicture.bind(this)}
+            >
+            Capture
+          </Text>
+        </Camera>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+})
+
+export default ImageCapture
